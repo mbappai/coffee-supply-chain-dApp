eaa9e08ce88e29f81aa1375eb00094684ea1c25e
mujeex <mujahid.bappai@yahoo.com>
2022-03-30 16:06:08 +0100

    feat: Setup rinkeby network with hd-wallet and dotenv variables

--- truffle-config.js
+++ truffle-config.js
@@ -21,8 +21,9 @@
 // const HDWallet = require('truffle-hdwallet-provider');
 // const infuraKey = "fj4jll3k.....";
 //
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+ require('dotenv').config();
+
+ const HDWalletProvider = require("@truffle/hdwallet-provider");
 
 module.exports = {
   /**
@@ -44,14 +45,12 @@ module.exports = {
     //
     development: {
      host: "127.0.0.1",     // Localhost (default: none)
-     port: 9545,            // Standard Ethereum port (default: none)
+     port: 7545,            // Standard Ethereum port (default: none)
      network_id: "*",       // Any network (default: none)
     },
     
-    develop: {
-     host: "127.0.0.1",     // Localhost (default: none)
-     port: 9545,            // Standard Ethereum port (default: none)
-     network_id: "*", 
+    develop: {   // Localhost (default: none)
+     port: 7545,            // Standard Ethereum port (default: none)
     },
 
     // Another network with more advanced options...
@@ -64,16 +63,18 @@ module.exports = {
       // websockets: true        // Enable EventEmitter interface for web3 (default: false)
     // },
 
-    // Useful for deploying to a public network.
-    // NB: It's important to wrap the provider as a function.
-    // ropsten: {
-      // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
-      // network_id: 3,       // Ropsten's id
-      // gas: 5500000,        // Ropsten has a lower block limit than mainnet
-      // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-      // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-      // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    // },
+    rinkeby: {
+      provider: () => new HDWalletProvider( process.env.MNEMONIC_PHRASE,
+         `https://rinkeby.infura.io/v3/${process.env.INFURA_KEY}`),
+        // numberOfAddresses: 1,
+        from:'0x87906b0E252B370251E58730D7B88FdEEC9d9d8e',
+        network_id:4,
+        shareNonce: true,
+        derivationPath: "m/44'/1'/0'/0/",
+        networkCheckTimeoutnetworkCheckTimeout: 10000,
+        timeoutBlocks: 200
+      ,
+    },
 
     // Useful for private networks
     // private: {
