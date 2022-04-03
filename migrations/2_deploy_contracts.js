8e56c05cef5ac8750403ba141d88cb4e3614f38a
mujeex <mujahid.bappai@yahoo.com>
2022-04-03 17:35:24 +0100

    fix: Modify artifact file path for supplyChain contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,7 +3,7 @@ var FarmerRole = artifacts.require("./FarmerRole.sol");
 var DistributorRole = artifacts.require("./DistributorRole.sol");
 var RetailerRole = artifacts.require("./RetailerRole.sol");
 var ConsumerRole = artifacts.require("./ConsumerRole.sol");
-var SupplyChain = artifacts.require("./SupplyChain.sol");
+var SupplyChain = artifacts.require("SupplyChain.sol");
 
 module.exports = function(deployer) {
   // deployer.deploy(FarmerRole);
